1a477f59a739e872523084febbea1da92d5ac657
ana11090 <72621705+ana11090@users.noreply.github.com>
2023-10-19 18:03:10 +0300

    Adaugare controller

---
+++ Niculae_Ana-Maria_Lab1/Controllers/LaboratorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Niculae_Ana_Maria_Lab1.Controllers
+{
+    public class LaboratorController : Controller
+    { 
+
+        public IActionResult Index()
+        {
+            string mesajBunVenit = "Bine ați venit!"; // Mesajul de bun venit pe care dorești să-l afișezi
+            return Content(mesajBunVenit);
+        }
+
+        public IActionResult SalutUtilizator()
+        {
+            string salut = "Salut!";
+            return Content(salut);
+        }
+
+        public IActionResult MesajCuParametri(string text, int numar)
+        {
+            string mesaj = $"Textul introdus este: {text}, iar numărul este: {numar}";
+            return Content(mesaj);
+        }
+    }
+}
